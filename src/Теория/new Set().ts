38a2476680831d9984a47568eab91ce009b4e805
aleksei.vedernikov <templar_sc@mail.ru>
2026-05-06 21:15:59 +0300

    теория массивов и new Set()

---
+++ src/Теория/new Set().ts
@@ -0,0 +1,6 @@
+export {};
+// Множество Set() может принимать строку в качестве аргумента:
+// Теория: https://bobbyhadz.com/blog/javascript-initialize-set-with-values
+
+const set = new Set('hello');
+console.log(set); // 👉️ {'h', 'e', 'l', 'o'}
