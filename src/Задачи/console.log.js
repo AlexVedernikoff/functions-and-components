26182e110faf63248256536eb5626f9f116ee631
aleksei.vedernikov <templar_sc@mail.ru>
2026-08-04 04:57:50 +0300

    задача на promise и console.log

--- src/Задачи/console.log.js
+++ src/Задачи/console.log.js
@@ -36,3 +36,31 @@ a.sayLastName.bind({lastName: 'Petrov'})(); // undefined
 // undefined
 
 // ************************************************************
+// *** Задача 2 ***********************************************
+
+console.log('start'); //
+setTimeout(() => console.log('timeout'), 0); //
+new Promise((resolve, reject) => {
+	console.log('promise constructor'); //
+	reject();
+})
+	.then(() => console.log('promise')) //
+	.catch(() => console.log('promise1')) //
+	.catch(() => console.log('promise2')) //
+	.then(() => console.log('promise3')) //
+	.then(() => console.log('promise4')); //
+console.log('final'); //
+
+// синхронный код: 'start' 'promise constructor' 'final'
+// микрозадачи: 'promise1' 'promise3' 'promise4'
+// макрозаачи: 'timeout'
+
+// 'start'
+// 'promise constructor'
+// 'final'
+// 'promise1'
+// 'promise3'
+// 'promise4'
+// 'timeout'
+
+// ************************************************************
