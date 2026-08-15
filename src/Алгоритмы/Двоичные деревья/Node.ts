0ac007453f3244eb0e8bdc75fca5b1e171b463ba
aleksei.vedernikov <templar_sc@mail.ru>
2026-08-15 20:51:34 +0300

    class Node

--- src/Алгоритмы/Двоичные деревья/Node.ts
+++ src/Алгоритмы/Двоичные деревья/Node.ts
@@ -2,9 +2,21 @@ export class Node {
 	public value: number;
 	public left: Node | null;
 	public right: Node | null;
-	constructor(value: number) {
+	constructor(value: number, left: Node | null = null, right: Node | null = null) {
 		this.value = value;
-		this.left = null;
-		this.right = null;
+		this.left = left;
+		this.right = right;
 	}
 }
+
+// prettier-ignore
+const tree = new Node(5, 
+		new Node(3, new Node(2), new Node(4)), 
+		new Node(7, new Node(6), new Node(8))
+	);
+
+console.log('tree = ', tree);
+
+//              5
+//          3        7
+//        2   4    6   8
